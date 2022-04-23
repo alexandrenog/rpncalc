d01dc6bcc0ecd954546c708a1e1a0d675f2801f0
Alexandre Nogueira <alexandre.nog212@gmail.com>
2022-04-23 00:09:09 -0300

    Update main.cr

--- main.cr
+++ main.cr
@@ -270,7 +270,7 @@ end
 
 RPNCalc.new.start
 
-# exemple of expressions:
+# example of expressions:
 # { dup doif__opo } abs
 # { 2 cpyn - dup abs dup doif_/_pop } greater
 # { swap greater } lesser or { greater opo } lesser
